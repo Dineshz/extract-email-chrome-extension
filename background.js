b3c2d3de2eeab511edbe6653efbaccd7bfd907e5
Dinesh <dineshkumarz.cool@gmail.com>
2018-01-04 17:33:49 +0530

    Social links

--- background.js
+++ background.js
@@ -1,22 +1,83 @@
 // background.js
 
+const emptyObj = {pattern: "", secondaryPattern: "", result: []}
+const emptyArray = []
+
+const resetStorage = () => {
+  localStorage.setItem("regex_search", JSON.stringify(emptyObj))
+  localStorage.setItem("social_links", JSON.stringify(emptyArray))
+}
+
 chrome.browserAction.onClicked.addListener((tab) => {
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-    let activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "search"});
-  });
-});
+    let activeTab = tabs[0]
+    chrome.tabs.sendMessage(activeTab.id, {
+      message: 'search',
+      pattern: '([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)',
+    })
+    chrome.tabs.sendMessage(activeTab.id, {
+      message: 'getSocialLinks',
+    })
+  })
+})
 
 chrome.runtime.onMessage.addListener(
   (request, sender, sendResponse) => {
     switch(request.message) {
-      case "searchResults": {
-        chrome.storage.sync.set({
-          result: request.result
-        });
-        break;
+      case 'searchResults': {
+        const {pattern, secondaryPattern, result} = request
+        let storedItem = localStorage.getItem("regex_search") || "{}"
+        try {
+          storedItem = JSON.parse(storedItem)
+        } catch(e) {
+          //
+        }
+        if(typeof storedItem === "object") {
+          storedItem.pattern = pattern;
+          storedItem.secondaryPattern = secondaryPattern
+          if(storedItem.result && storedItem.result.constructor === Array)
+            storedItem.result = Array.from(new Set(storedItem.result.concat(result)))
+          else storedItem.result = result
+        }
+        localStorage.setItem("regex_search", JSON.stringify(storedItem))
+        break
       }
+      case 'socialResults': {
+        let storedItem = localStorage.getItem("social_links") || "[]"
+        storedItem = JSON.parse(storedItem);
+        localStorage.setItem("social_links", JSON.stringify(storedItem.concat(request.socialLinks)))
+        break
+      }
+      default: break
     }
   }
-);
+)
+
+chrome.runtime.onMessageExternal.addListener(
+  (request, sender, sendResponse) => {
+    switch(request.message) {
+
+      case 'getSearchResults': {
+        sendResponse({data: localStorage.getItem("regex_search")})
+        break
+      }
+
+      case 'resetSocialLinks':
+      case 'resetSearchResults': {
+        resetStorage()
+        break
+      }
+
+      case 'getSocialLinks': {
+        sendResponse({data: localStorage.getItem("social_links")})
+        break
+      }
+
+      default: break;
+    }
+  }
+)
+//
+// window.onunload = resetStorage
+// chrome.windows.onRemoved.addListener(resetStorage)
--- content.js
+++ content.js
@@ -1,5 +1,4 @@
 
-let matchedText = [];
 const MAX_RESULTS = 500;
 const ELEMENT_NODE_TYPE = 1;
 const TEXT_NODE_TYPE = 3;
@@ -14,7 +13,7 @@ const isVisible = (node) => node && (
 /* Validate that a given pattern string is a valid regex */
 const validateRegex = (pattern='') => {
   try {
-    let regex = new RegExp(pattern, 'i');
+    let regex = new RegExp(pattern, 'gi');
     return regex;
   } catch(e) {
     return false;
@@ -28,52 +27,87 @@ const isTextNode = (node) => node && node.nodeType === TEXT_NODE_TYPE
 const isExpandable = (node) => node && node.nodeType === ELEMENT_NODE_TYPE
   && node.childNodes && !UNEXPANDABLE.test(node.tagName) && isVisible(node)
 
+/* Get all links from page */
+const getAllLinks = () => {
+  let links = []
+  $('a').each(function(){
+    let hrefl = $(this).attr('href');
+    links.push(hrefl)
+  })
+  return links
+}
+
+/* Get social links from page */
+const getSocialLinks = () => {
+  const isSocial = (link) => {
+    const socialPatterns = [
+      /^(https?:\/\/)?(www\.)?facebook.com\/[a-zA-Z0-9(\.\?)?]/,
+      /^(https?:\/\/)?(www\.)?twitter.com\/[a-zA-Z0-9(\.\?)?]/,
+      /^(https?:\/\/)?(www\.)?instagram.com\/[a-zA-Z0-9(\.\?)?]/,
+      /^(https?:\/\/)?(www\.)?linkedin.com\/[a-zA-Z0-9(\.\?)?]/,
+    ]
+    return socialPatterns.some(pattern => pattern.test(link))
+  }
+  return getAllLinks().filter(isSocial);
+}
+
 /* Get text that matches regex */
-const getMatches = (regex, maxResults=MAX_RESULTS) => {
-  const getMatchRecursive = (node) => {
-    if(matchedText.length >= maxResults){
-      return;
-    }
-    if (isTextNode(node)) {
-      let index = node.data.search(regex);
-      if (index >= 0 && node.data.length > 0) {
-        let match = node.data.match(regex)[0];
-        if(match) {
-          let extractedEmails = match.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi)
-          matchedText = matchedText.concat(extractedEmails);
-        }
-        return 1;
+const getAllText = () => {
+  let wholeText = ''
+  const getTextRecursive = (node) => {
+    if (isTextNode(node)) { wholeText = wholeText + node.data }
+    else if (isExpandable(node)) {
+      let children = node.childNodes;
+      for (let i = 0; i < children.length; ++i) {
+        let child = children[i];
+        i += getTextRecursive(child);
       }
-    } else if (isExpandable(node)) {
-        let children = node.childNodes;
-        for (let i = 0; i < children.length; ++i) {
-          let child = children[i];
-          i += getMatchRecursive(child);
-        }
     }
     return 0;
   }
-  matchedText = [];
-  getMatchRecursive(document.getElementsByTagName('body')[0]);
+  getTextRecursive(document.getElementsByTagName('body')[0])
+  return wholeText;
 };
 
-const search = (regexString, maxResults) => {
-  let regex = validateRegex(regexString);
-  if(regex && regex!=='') getMatches(regex, maxResults);
-  return matchedText;
-}
+const search = (regex) => getAllText()
+  .replace(new RegExp(String.fromCharCode(160), "g"), " ")
+  .split(" ")
+  .map(word => word.match(regex))
+  .filter(value => value)
+  .reduce((a, b) => a.concat(b), [])
+
+const dedupe = (values) => Array.from(new Set(values))
 
 chrome.runtime.onMessage.addListener(
   (request, sender, sendResponse) => {
     switch(request.message) {
-      case "search": {
-        let result = search("(.+)@(.+){2,}\.(.+){2,}", MAX_RESULTS)
-        console.log("result", result);
-        chrome.runtime.sendMessage({
-          message: "searchResults",
+      case 'search': {
+        const {pattern} = request
+        let result = []
+        let regex = validateRegex(pattern)
+        if(regex) result = dedupe(search(regex))
+        else result = ["invalid pattern"]
+        let resultObj = {
+          message: 'searchResults',
+          pattern,
           result
-        });
+        };
+        console.log("regex search", resultObj)
+        chrome.runtime.sendMessage(resultObj)
+        break
       }
+
+      case 'getSocialLinks': {
+        let resultObj = {
+          message: 'socialResults',
+          socialLinks: dedupe(getSocialLinks()),
+        }
+        console.log("social links", resultObj)
+        chrome.runtime.sendMessage(resultObj)
+        break
+      }
+
+      default: break
     }
   }
 );
